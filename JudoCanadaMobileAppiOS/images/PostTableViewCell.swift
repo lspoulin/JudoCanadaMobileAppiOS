d508c35168a9a17a14400fd1898274d7fe88492d
Unknown <lspoulin@gmail.com>
2018-05-07 14:08:12 -0400

    Successfully implementing a TableViewController with the live posts

---
+++ JudoCanadaMobileAppiOS/images/PostTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostTableViewCell.swift
+//  
+//
+//  Created by Louis-Simon Poulin on 2018-05-07.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelExerpt: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
