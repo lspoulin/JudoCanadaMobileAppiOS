4a97e96ab1616544496c907a36117a962f369bd2
lspoulin <lspoulin@gmail.com>
2018-05-07 09:21:16 -0400

    Initial Commit

---
+++ JudoCanadaMobileAppiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JudoCanadaMobileAppiOS
+//
+//  Created by Louis-Simon Poulin on 2018-05-07.
+//  Copyright © 2018 Louis-Simon Poulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
